a0ff4fc0e02580c30cc411f0a842ea541ac91099
enyene akpan <akpanenyene@gmail.com>
2022-10-18 05:20:52 -0700

    solution sumof integers

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * add - adds two integers
+ * @a: integer one
+ * @b: integer two
+ * Return: sum of a and b
+ */
+
+int add(int a, int b)
+{
+	int c;
+
+	c = a + b;
+	return (c);
+}
