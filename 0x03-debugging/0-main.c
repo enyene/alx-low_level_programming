356a4c592ec9e7fc2a5c1ed1abcbe3da3a9d82f0
enyene akpan <akpanenyene@gmail.com>
2022-10-19 05:20:01 -0700

    solution text for postive integer

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * main - test function that prints if
+ * an integer id positive or not
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i;
+
+	i = 0;
+	positive_or_negative(i);
+
+	return (0);
+}
