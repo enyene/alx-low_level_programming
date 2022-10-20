e0c566a4efc1225226fc6618762fb49034fdf2aa
enyene akpan <akpanenyene@gmail.com>
2022-10-20 04:30:51 -0700

    update

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -2,7 +2,6 @@
 
 /**
  * print_numbers - prints the numbers from 0 to 9
- * Return: 0
  */
 
 void print_numbers(void)
@@ -14,5 +13,4 @@ void print_numbers(void)
 		_putchar(i);
 	}
 	_putchar("\n");
-	return (0);
 }
