38d351b5601eb2b9a2219d143af82b8acac07d94
enyene akpan <akpanenyene@gmail.com>
2022-11-02 05:05:10 -0700

    solution prime numbers

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+int actual_prime(int n, int i);
+
+/**
+ * is_prime_number - says if an integer is prime
+ * @n: number to test
+ *
+ * Return: 1 if prime
+ */
+
+int is_prime_number(int n)
+{
+	if (n <= 1)
+		return (0);
+	return (actual_prime(n, n - 1));
+}
+
+/**
+ * actual_prime - calculates primes
+ * @n: number
+ * @i: iterator
+ *
+ * Return: 1 if n is prime
+ */
+
+int actual_prime(int n, int i)
+{
+	if (i == 1)
+		return (1);
+	if (n % i == 0 && i > 0)
+		return (0);
+	return (actual_prime(n, i - 1));
+}
